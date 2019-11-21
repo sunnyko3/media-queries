2458b4e7f8dfe69dd4b30248c0d1b8783ec809f5
sunnyko3 <57108786+sunnyko3@users.noreply.github.com>
2019-11-20 17:25:26 -0700

    media-queries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
+library.add(faBars);
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+    <div id="top-half">
+      <header>
+        <logo>Start Bootstrap</logo>
+        <nav>
+          <a>Services</a>Portfolio<a>About</a><a>Team</a>Contact<a></a><a></a>
+        </nav>
       </header>
+      <div id="intro">
+          <h2>Welcome To Our Studio!</h2>
+          <h1>IT'S NICE TO MEET YOU</h1>
+          <div><p id="p">Tell Me More</p></div>
+      </div>
     </div>
+  </body>
   );
 }
 
